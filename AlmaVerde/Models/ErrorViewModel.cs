84dfaf4752c409e2bc1a69961195532cc92c9412
Kesley de Mello <kesley_ksl@hotmail.com>
2019-07-11 22:03:23 -0300

    Adicionar arquivos de projeto.

---
+++ AlmaVerde/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlmaVerde.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
